bfd5ca1474756190a6d0a56af04844bda0f0b769
Fatih Mehmet Albayrak <250542001@firat.edu.tr>
2025-10-23 21:47:04 +0300

    Update IsimKarti.java

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,13 @@
+public class OgrenciKart {
+    public static void main(String[] args) {
+        String isim = "Fatih Mehmet";
+        String soyisim = "Albayarak";
+        String ogrenciNo = "250542001";
+        String bolum = "Yazilim Muhendisligi";
+
+        System.out.println(isim);
+        System.out.println(soyisim);
+        System.out.println(ogrenciNo);
+        System.out.println(bolum);
+    }
+}

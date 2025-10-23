e39dd9906b30e28565bd382251aa6fc1563f3de2
Fatih Mehmet Albayrak <250542001@firat.edu.tr>
2025-10-23 21:58:53 +0300

    Update MilKilometreTablosu.java

---
+++ Task-5-TabloDesenleri/MilKilometreTablosu.java
@@ -0,0 +1,13 @@
+public class MilToKm {
+    public static void main(String[] args) {
+        System.out.println("Mil\tKilometre");
+        System.out.println("-----------------");
+        
+        int[] miller = {1, 5, 10, 20, 50};
+        
+        for (int mil : miller) {
+            double km = mil * 1.609;
+            System.out.println(mil + "\t" + km);
+        }
+    }
+}

eff986bb87dca97c357dabbec23cb9a89688079b
Fatih Mehmet Albayrak <250542001@firat.edu.tr>
2025-10-23 21:52:55 +0300

    Update AsciiArtHarf.java

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,9 @@
+public class AsciiF {
+    public static void main(String[] args) {
+        System.out.println("FFFFFFF");
+        System.out.println("F");
+        System.out.println("FFFFFF");
+        System.out.println("F");
+        System.out.println("F");
+    }
+}

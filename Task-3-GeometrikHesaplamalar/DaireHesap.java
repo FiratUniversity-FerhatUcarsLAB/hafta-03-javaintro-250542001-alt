65ed602710594b29f100fa014c57b0f6654bfb3e
Fatih Mehmet Albayrak <250542001@firat.edu.tr>
2025-10-23 21:21:56 +0300

    Update DaireHesap.java

---
+++ Task-3-GeometrikHesaplamalar/DaireHesap.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double yaricap = 5.5;
+        double alan = Math.PI * yaricap * yaricap;
+        double cevre = 2 * Math.PI * yaricap; 
+
+        System.out.println("Yaricap: " + yaricap);
+        System.out.println("Alan: " + alan);
+        System.out.println("Cevre: " + cevre);
+    }
+}

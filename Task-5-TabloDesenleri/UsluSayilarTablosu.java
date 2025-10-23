35f004c040c5fe54c2e6eecf97ff7b8ccc6a1cc0
Fatih Mehmet Albayrak <250542001@firat.edu.tr>
2025-10-23 22:02:49 +0300

    Update UsluSayilarTablosu.java

---
+++ Task-5-TabloDesenleri/UsluSayilarTablosu.java
@@ -0,0 +1,9 @@
+public class KuvvetTablosu {
+    public static void main(String[] args) {
+        System.out.println("a\t a^2\t a^3");
+        System.out.println("-----------------");
+        for (int a = 1; a <= 5; a++) {
+            System.out.println(a + "\t " + (a * a) + "\t " + (a * a * a));
+        }
+    }
+}
